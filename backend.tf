019ea22d18450724bf8c26678e6d6e125fd73ffb
Zachary Lim <zachary_limjunsheng@hotmail.com>
2026-05-21 22:17:06 +0800

    updated backend.tf to latest bucket

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
   backend "s3" {
     bucket = "sctp-ce12-tfstate-bucket"
     key    = "zaclim-sctp-ce12-3.6-snyk-scan.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
-    region = "us-east-1"
+    region = "ap-southeast-1"
   }
 }
